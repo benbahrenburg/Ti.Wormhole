7cba5c5facce3758e30102dbd45f31b954fe8fab
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-01 13:23:55 +0100

    Minor refactoring, fix clearAllMessageContents

--- iphone/Classes/TiWormholeModule.m
+++ iphone/Classes/TiWormholeModule.m
@@ -16,124 +16,96 @@
 #pragma mark Internal
 
 // this is generated for your module, please do not change it
--(id)moduleGUID
+- (id)moduleGUID
 {
 	return @"a27f3702-c2d4-4ae9-a575-b17a25926e24";
 }
 
 // this is generated for your module, please do not change it
--(NSString*)moduleId
+- (NSString *)moduleId
 {
 	return @"ti.wormhole";
 }
 
-#pragma mark Lifecycle
-
--(void)startup
-{
-	// you *must* call the superclass
-	[super startup];
-
-}
-
-
--(void)shutdown:(id)sender
-{
-	// you *must* call the superclass
-	[super shutdown:sender];
-}
-
-#pragma mark Cleanup
-
-
-#pragma mark Internal Memory Management
-
--(void)didReceiveMemoryWarning:(NSNotification*)notification
-{
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
-}
-
-
 #pragma Public APIs
 
--(void)start:(id)args
+- (void)start:(id)args
 {
     ENSURE_UI_THREAD(start, args)
     ENSURE_SINGLE_ARG(args,NSDictionary);
-    ENSURE_TYPE(args,NSDictionary);
-    if(![args objectForKey:@"suiteName"]) {
+    
+    if (![args objectForKey:@"suiteName"]) {
         NSLog(@"[ERROR] suiteName field is required");
         return;
     }
+    
     NSString* identifier =[TiUtils stringValue:@"suiteName" properties:args];
     NSString* dir = [TiUtils stringValue:@"directory" properties:args def:@"tiwormhole"];
-    self.wormhole = [[MMWormhole alloc] initWithApplicationGroupIdentifier:identifier
-                                                         optionalDirectory:dir];
+    
+    [self setWormhole:[[MMWormhole alloc] initWithApplicationGroupIdentifier:identifier
+                                                         optionalDirectory:dir]];
 }
 
--(void)post:(id)args
+- (void)post:(id)args
 {
+    ENSURE_UI_THREAD(post,args);
     ENSURE_ARG_COUNT(args,2);
     NSString* eventName = [TiUtils stringValue:[args objectAtIndex:0]];
     NSDictionary *dict = [args objectAtIndex:1];
     ENSURE_TYPE(dict,NSDictionary);
-    ENSURE_UI_THREAD(post,args);
-    [self.wormhole passMessageObject:dict identifier:eventName];
+    
+    [[self wormhole] passMessageObject:dict identifier:eventName];
 }
 
 
--(id)readWormhole:(id)args
+- (id)readWormhole:(id)args
 {
-    ENSURE_ARG_COUNT(args,1);
-    NSString* name = [TiUtils stringValue:[args objectAtIndex:0]];
-    ENSURE_TYPE(name,NSString);
+    ENSURE_SINGLE_ARG(args, NSString);
+    id messageObject = [[self wormhole] messageWithIdentifier:args];
     
-    id messageObject = [self.wormhole messageWithIdentifier:name];
-    
-    if([messageObject isKindOfClass: [NSDictionary class]]){
-    
-        return  [NSDictionary dictionaryWithObjectsAndKeys:
-                                       name,@"wormholeName",
-                                       (NSDictionary*)messageObject,@"wormhole",
-                                       nil];
-        
-    } else if ([messageObject isKindOfClass: [NSArray class]]){
-        return  [NSDictionary dictionaryWithObjectsAndKeys:
-                     name,@"wormholeName",
-                     (NSArray*)messageObject,@"wormhole",
-                     nil];
-    }else{
+    if ([messageObject isKindOfClass: [NSDictionary class]]) {
+        return @{
+            @"wormhole": (NSDictionary *)messageObject,
+            @"wormholeName": args
+        };
+    } else if ([messageObject isKindOfClass: [NSArray class]]) {
+        return @{
+            @"wormhole": (NSArray *)messageObject,
+            @"wormholeName": args
+        };
+    } else {
         return nil;
     }
 }
 
--(void)addWormhole:(id)args
+- (void)addWormhole:(id)args
 {
-    ENSURE_ARG_COUNT(args,2);
+    ENSURE_ARG_COUNT(args, 2);
+    
     NSString* eventName = [TiUtils stringValue:[args objectAtIndex:0]];
     KrollCallback *callback = [args objectAtIndex:1];
-    ENSURE_TYPE(callback,KrollCallback);
-    ENSURE_UI_THREAD(addWormhole,args);
     
-    [self.wormhole listenForMessageWithIdentifier:eventName listener:^(id messageObject) {
-        if([messageObject isKindOfClass: [NSDictionary class]]){
-            if(callback){
-                NSDictionary *dictEvent = [NSDictionary dictionaryWithObjectsAndKeys:
-                                           eventName,@"wormholeName",
-                                           (NSDictionary*)messageObject,@"wormhole",
-                                         nil];
+    ENSURE_TYPE(callback,KrollCallback);
+    ENSURE_UI_THREAD(addWormhole, args);
+    
+    [[self wormhole] listenForMessageWithIdentifier:eventName listener:^(id messageObject) {
+        if ([messageObject isKindOfClass: [NSDictionary class]]) {
+            if (callback) {
+                NSDictionary *dictEvent = @{
+                    @"wormhole": (NSDictionary *)messageObject,
+                    @"wormholeName": eventName
+                };
                 [self _fireEventToListener:eventName
                                 withObject:dictEvent listener:callback thisObject:nil];
             }
         }
-        if([messageObject isKindOfClass: [NSArray class]]){
-            if(callback){
-                NSDictionary *arrayEvent = [NSDictionary dictionaryWithObjectsAndKeys:
-                                           eventName,@"wormholeName",
-                                           (NSArray*)messageObject,@"wormhole",
-                                           nil];
+        
+        if ([messageObject isKindOfClass: [NSArray class]]) {
+            if (callback) {
+                NSDictionary *arrayEvent = @{
+                    @"wormhole": (NSArray *)messageObject,
+                    @"wormholeName": eventName
+                };
                 [self _fireEventToListener:eventName
                                 withObject:arrayEvent listener:callback thisObject:nil];
             }
@@ -142,17 +114,18 @@
     }];
 }
 
--(void)removeWormhole:(id)arg
+- (void)removeWormhole:(id)arg
 {
-    ENSURE_ARG_COUNT(arg,1);
-    ENSURE_UI_THREAD(removeWormhole,arg);
+    ENSURE_ARG_COUNT(arg, 1);
+    ENSURE_UI_THREAD(removeWormhole, arg);
+    
     NSString* eventName = [TiUtils stringValue:[arg objectAtIndex:0]];
-    [self.wormhole stopListeningForMessageWithIdentifier:eventName];
+    [[self wormhole] stopListeningForMessageWithIdentifier:eventName];
 }
 
--(void)clearAllMessageContents:(id)unused
+- (void)clearAllMessageContents:(id)unused
 {
-    [self wormhole].clearAllMessageContents;
+    [[self wormhole] clearAllMessageContents];
 }
 
 @end
